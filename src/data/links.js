70a3e679a4feed7c027aa6716f27f30898fba0eb
Houtan Ghaebi <houtan.ghaebi@gmail.com>
2026-07-26 21:23:08 +0100

    Add LinkedIn profile link

--- src/data/links.js
+++ src/data/links.js
@@ -1,6 +1,6 @@
 export const links = {
   github: "https://github.com/HoutanGh",
-  linkedin: null,
+  linkedin: "https://www.linkedin.com/in/houtan-ghaebi-a56215198",
   email: "mailto:houtan.ghaebi@gmail.com",
   cv: `${import.meta.env.BASE_URL}cv/houtan-ghaebi-cv.pdf`,
 };
