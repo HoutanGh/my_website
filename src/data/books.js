c6efb533b5a73dea8befd6f2164389713dd7d5dc
HoutanGh <houtan.ghaebi@gmail.com>
2026-08-01 16:18:17 +0100

    Update CV and reading reflections

--- src/data/books.js
+++ src/data/books.js
@@ -1,18 +1,16 @@
-const thoughtPlaceholder = "Personal note coming soon.";
-
 export const books = [
   {
     status: "CURRENT",
     title: "The Hobbit",
     author: "J. R. R. Tolkien",
-    thought: "I should up my riddle game, too risky not to.",
+    thought: "I should up my riddle game.",
   },
   {
     status: "CURRENT",
     title: "Man and His Symbols",
     author: "C. G. Jung",
     thought:
-      "Find the actual patient examples given by Jung very interesting, sceptic of the divine nature of dreams but it was quite eye opening when he spoke about physical conditions and their condition to unaddressed unconsciousness. Still on Jung's section.",
+      "I find the actual patient examples given by Jung very interesting, looking for anything that I can connect with myself. I am sceptic of the divine nature of dreams but it was quite eye opening when he spoke about physical conditions and their connection to the unaddressed unconsciousness. Currently still on Jung's section.",
   },
   {
     status: "CURRENT",
@@ -20,43 +18,48 @@ export const books = [
     author: "Samad Behrangi",
     rtl: true,
     thought:
-      "The lack of vowels and ezafes in the words makes it very difficult to learn. It becomes more about knowing as many words as possible rather than simply knowing the alphabet and learning from there.",
+      "The lack of vowels (ezafes) in the words makes it very difficult to learn. It becomes more about knowing as many words as possible rather than simply knowing the alphabet and learning from there.",
   },
   {
     status: "LISTENING",
     title: "Bible",
     author: "—",
     thought:
-      "Trying to go through it as one would go through Homer's Odyssey—a deeply enriched work of literature.",
+      "Trying to go through it as one would go through Homer's Odyssey - a deeply enriched work of literature.",
   },
   {
     status: "PARKED",
     title: "Don Quixote",
     author: "Miguel de Cervantes",
-    thought: thoughtPlaceholder,
+    thought:
+      "Slowed down my reading, as I've read that in those days excerpts of books like this were released periodically, weekly or monthly in articles, like TV shows nowadays. So it shouldn't really be read as an entire block of text.",
   },
   {
     status: "PARKED",
     title: "The Gathas",
     author: "Zarathustra",
-    thought: thoughtPlaceholder,
+    thought:
+      "Barely started.",
   },
   {
     status: "READ",
     title: "Notes from Underground",
     author: "Fyodor Dostoevsky",
-    thought: thoughtPlaceholder,
+    thought:
+      "I figure like most people, the initial part where the underground man spirals in his own thoughts, I found the most interesting.",
   },
   {
     status: "READ",
     title: "Crime and Punishment",
     author: "Fyodor Dostoevsky",
-    thought: thoughtPlaceholder,
+    thought:
+      "Only truly understand what I was reading when I watched a review from Unsolicited advice.",
   },
   {
     status: "READ",
     title: "Animal Farm",
     author: "George Orwell",
-    thought: thoughtPlaceholder,
+    thought:
+      "Honestly, it is a truly disturbing book. I can't believe this was one of the books we went through in Year 9.",
   },
 ];
